f88257fa7024d5214b8b6dae2216767d239f4ba9
Gian Martín Tituaña Intriago <mtituanai@estud.usfq.edu.ec>
2025-03-11 22:55:19 -0500

    Create echo.sh

---
+++ echo.sh
@@ -0,0 +1,1 @@
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/<version>/multiverse amd64 packages" | sudo tee /etc/apt/sources.list.d/mongodb-org-<version>.list
